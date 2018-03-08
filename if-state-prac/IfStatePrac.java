2248282e9df6a5eb110162b0e2b17c1556fdfe00
EChin4 <36395509+EChin4@users.noreply.github.com>
2018-03-08 11:08:11 -0500

    Add files via upload

---
+++ if-state-prac/IfStatePrac.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class IfStatePrac {
+    public static void main(String[] args) {
+        /*double mark = 0;
+        
+        if (mark > 50) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        } 
+        
+        int x = 9;
+        int y = 8;
+        
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else if (y > x) {
+            System.out.println("y is greater than x");
+        } else {
+            System.out.println("x is equal to y");
+        } 
+    
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println();
+        System.out.print("wassyoage? ");
+        int age = input.nextInt();
+        
+        if (age >= 65) {
+            System.out.println("senior");
+        } else if (age < 65 && age >= 18) {
+            System.out.println("regular admission");
+            if (age == 64) {
+                System.out.println("but you are so close, buddy.");
+            }
+        } else if (age < 18 && age > 10) {
+            System.out.println("child");
+        } else {
+            System.out.println("free");
+        } */
+        
+        System.out.println("__\\_");
+    }
+} 
+   
---
+++ sketchy-games/TheJomieGame.java
@@ -0,0 +1,97 @@
+// Thurs March 8th, 2018
+// in progress
+import java.util.Scanner;
+
+public class TheJomieGame {
+    public static void main(String[] args) {
+        // Note:
+        /* System.out.println("\\"); need to type in \\ to output just \ 
+        System.out.println("/");
+        System.out.println("\\/"); */
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("THE JOMIE GAME!");
+        System.out.println();
+        System.out.print("Are you ready to start? ");
+        String readyStart = input.next();
+        
+        System.out.println();
+        System.out.println(" --------------");
+        System.out.println("|   _      _   |");
+        System.out.println("|  |0|    |0|  |");
+        System.out.println("|   -  &   -   |");
+        System.out.println("|     \\__/     |");
+        System.out.println("|              |");
+        System.out.println(" --------------");
+        System.out.println();
+        
+        System.out.println("This is Jomie. Would you like to...");
+        System.out.println("    1. feed");
+        System.out.println("    2. compliment");
+        System.out.println("    3. befriend");
+        System.out.println("    4. punch");
+        System.out.println("    5. starve (Jomie)");
+        System.out.println("    6. all of the above");
+        System.out.println();
+        System.out.println("Type in the #!" );
+        int actionNum = input.nextInt();
+        
+        if (actionNum == 1) {
+            System.out.println();
+            System.out.println(" --------------");
+            System.out.println("|  /\\      /\\  |  *crunch*");
+            System.out.println("| |yum|  |yum| |    *CRUNCH*");
+            System.out.println("|   -  &   -   |");
+            System.out.println("|   \\______/   |");
+            System.out.println("|    \\____/    |");
+            System.out.println(" --------------");
+            System.out.println();
+        } else if (actionNum == 2) {
+            System.out.println();
+            System.out.println(" --------------");
+            System.out.println("|              |   <3");
+            System.out.println("|   =      =   |");
+            System.out.println("|   -  &   -   |");
+            System.out.println("|      \\/      |");
+            System.out.println("|              |");
+            System.out.println(" --------------");
+            System.out.println();
+        } else if (actionNum == 3) {
+            System.out.println();
+            System.out.println(" --------------  !");
+            System.out.println("|   ~     ~    |");
+            System.out.println("|  |* |   |* | |  id rather");
+            System.out.println("|   -  &   -   |    not tho");
+            System.out.println("|     /---\\    |"); 
+            System.out.println("|              |");
+            System.out.println(" --------------");
+            System.out.println();
+        } else if (actionNum == 4) {
+            System.out.println();
+            System.out.println(" --------------");
+            System.out.println("|   _      _   | no effect");
+            System.out.println("|  |0|    |0|  |");
+            System.out.println("|   -  &   -   |");
+            System.out.println("|     \\__/     |  im friggin strong");
+            System.out.println("|              |");
+            System.out.println(" --------------");
+            System.out.println();
+        } else if (actionNum == 5) {
+            System.out.println();
+            System.out.println(" ---  ok");
+            System.out.println("|o o|");
+            System.out.println(" ---");
+            System.out.println();
+        } else if (actionNum == 6) {
+            System.out.println();
+            System.out.println(" --------------");
+            System.out.println("|   _      _   |");
+            System.out.println("|  |?|    |?|  |");
+            System.out.println("|   -  &   -   |");
+            System.out.println("|     \\__/     |");
+            System.out.println("|              |");
+            System.out.println(" --------------");
+            System.out.println();
+        }
+    }
+}
